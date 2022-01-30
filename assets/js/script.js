2194c5a4b670e0790adb5400e78752ed0e7de8ad
Atticus-Robinson <acerobinson2@gmail.com>
2022-01-30 11:05:05 -0600

    save prior to callback async test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,87 @@
 var searchText;
-var data;
+var googleData, weatherData;
+const historyArray = [];
 
-function getWeatherInfo(latitude, longitude) {
+function getWeatherInfo(inputLocation) {
+    var coordinates = latLonConvert(inputLocation);
+    var latitude = coordinates.lat;
+    var longitude = coordinates.long;
+    console.log(latitude);
+    console.log(longitude);
     var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=bb91dbeea5be1f229728f70ee62875cc'
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
+            response.json().then(function (weatherData) {
+                console.log(weatherData);
             })
         }
     })
 }
 
-function latLonConvert(inputLocation) {
-    var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + inputLocation + '&key=AIzaSyD3SFsjzLRJjuS7MuUlRnfQT8kgo7iMIaQ'
-    fetch(apiUrl).then(function (response) {
+function latLonConvert(location) {
+    var googApi = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=AIzaSyD3SFsjzLRJjuS7MuUlRnfQT8kgo7iMIaQ'
+    fetch(googApi).then(function (response) {
         if (response.ok) {
-            response.json().then(function (data) {
-                data.results[0].address_components.forEach(function(item) {
-                    switch (item.types[0]) {
-                        case 'locality':
-                            city = item.long_name;
-                            break;
-                        case 'administrative_area_level_1':
-                            admin1 = item.long_name;
-                            break;
-                        case 'country':
-                            country = item.short_name;
-                            break;
-                        default:
-                            break;
-                    }
-                })
-                var lat = data.results[0].geometry.location.lat;
-                var long = data.results[0].geometry.location.lng;
-                createHistoryItem(city, admin1, country);
-                getWeatherInfo(lat, long);
+            response.json().then(function (googleData) {
+                var lat = googleData.results[0].geometry.location.lat;
+                var long = googleData.results[0].geometry.location.lng;
+                console.log(lat, long);
+                return {lat, long};
             })
         }
     })
 }
 
-function createHistoryItem(admin1, admin2, country) {
-    var check;
+function createHistoryItem(inputText) {
+    console.log(googleData);
+    /*var check;
     var same;
-    $('.search-history > li').each(function(index) {
+
+
+    data.results[0].address_components.forEach(function (item) {
+        switch (item.types[0]) {
+            case 'locality':
+                city = item.long_name;
+                break;
+            case 'administrative_area_level_1':
+                admin1 = item.long_name;
+                break;
+            case 'country':
+                country = item.short_name;
+                break;
+            default:
+                break;
+        }
+    })
+    $('.search-history > li').each(function (index) {
         check = ($(this).attr('id'));
         if (check === admin1) {
             same = false;
             return;
-        } else {
-        }
+        } else {}
     });
     if (same === false) return;
     var historyItem = $('<li>')
         .addClass('collection-item')
         .attr('id', admin1)
         .text(admin1 + ', ' + admin2 + ', ' + country);
-    
-    storeHistoryItem(admin1);
-    $('.search-history').prepend(historyItem);
+
+    $('.search-history').prepend(historyItem);*/
 }
 
 function storeHistoryItem(item) {
+    historyArray.unshift(item);
+    var string = JSON.stringify(historyArray);
+    localStorage.setItem("nepho-search-history", string);
+}
 
+function retieveHistory() {
+    var storage = localStorage.getItem("nepho-search-history");
+    var array = JSON.parse(storage);
+    array.forEach(function (currentValue) {
+        historyArray.push(currentValue);
+    })
+    console.log(historyArray);
 }
 
 function createInfoPage() {
@@ -79,7 +97,8 @@ $('#search-button').on('click', function () {
     searchText = $('#location').val();
     $('#location').val('');
     if (searchText) {
-        latLonConvert(searchText);
+        getWeatherInfo(searchText);
+        storeHistoryItem(searchText);
     }
 })
 
@@ -88,12 +107,14 @@ $('#location').keypress(function (e) {
         searchText = $('#location').val();
         $('#location').val('');
         if (searchText) {
-            latLonConvert(searchText);
+            getWeatherInfo(searchText);
+            storeHistoryItem(searchText);
         }
     }
 });
 
-$('.search-history').on('click', 'li', function(event) {
+$('.search-history').on('click', 'li', function (event) {
     event.preventDefault();
-    latLonConvert($(this).text());
 })
+
+retieveHistory();
