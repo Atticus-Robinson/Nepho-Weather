f360f556a7281bef773ae3b6aa62639687a266e7
Atticus-Robinson <acerobinson2@gmail.com>
2022-01-31 01:27:32 -0600

    added fade ins

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ async function getWeatherInfo(googleObject) {
 
     coordinates.lat = googleObject.results[0].geometry.location.lat;
     coordinates.long = googleObject.results[0].geometry.location.lng;
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + coordinates.lat + '&lon=' + coordinates.long + '&appid=bb91dbeea5be1f229728f70ee62875cc'
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + coordinates.lat + '&lon=' + coordinates.long + '&units=imperial&appid=bb91dbeea5be1f229728f70ee62875cc'
     var fetchResponse = await fetch(apiUrl)
     if (fetchResponse.ok) {
         var weatherData = await fetchResponse.json()
@@ -29,21 +29,23 @@ async function getWeatherInfo(googleObject) {
 async function displayWeather(text) {
     let googleData = await convert(text);
     createHistoryItem(googleData);
+    var locationName = googleData.results[0].formatted_address;
     let weatherData = await getWeatherInfo(googleData);
-    createInfoPage(weatherData);
-    create5Day(weatherData);
+    createInfoPage(weatherData, locationName);
+    create5Day(weatherData, locationName);
 }
 
 function createHistoryItem(googleDataObject) {
     var same = 0;
+    var i = 0;
     var shortArray = [];
+    var string = '';
     const object = {
         neighborhood: '',
         city: '',
         admin1: '',
         country: ''
     };
-    console.log(googleDataObject);
     googleDataObject.results[0].address_components.forEach(function (item) {
         switch (item.types[0]) {
             case 'neighborhood':
@@ -62,15 +64,21 @@ function createHistoryItem(googleDataObject) {
                 break;
         }
     })
-    if (object.neighborhood === '') {
-        var string = object.city + ', ' + object.admin1 + ', ' + object.country;
-    } else {
-        var string = object.neighborhood + ', ' + object.city + ', ' + object.admin1 + ', ' + object.country;
+    var displayArray = [object.neighborhood, object.city, object.admin1, object.country]
+    while (i < displayArray.length) {
+        if (i < (displayArray.length - 1)) {
+            if (displayArray[i]) {
+                string += displayArray[i] + ', ';
+            }
+        } else {
+            string += displayArray[i];
+        }
+        i++;
     }
+    console.log(string);
     $(".collection > li").each(function () {
         var check = ($(this).text());
         if (check === string) {
-            console.log('match');
             same++;
             return same;
         }
@@ -99,7 +107,7 @@ function storeHistoryItem(arrayItem) {
     localStorage.setItem('nepho-history', string);
 }
 
-function retieveHistory() {
+/*function retieveHistory() {
     var storage = localStorage.getItem('nepho-history');
     var array = JSON.parse(storage);
     array.forEach(function (item, index) {
@@ -110,14 +118,71 @@ function retieveHistory() {
     })
 
 
-}
+}*/
 
-function createInfoPage(weatherObject) {
-    //console.log(weatherObject);
+function createInfoPage(weatherObject, locationName) {
+    let timezone = weatherObject.timezone;
+    luxonObject = luxon.DateTime.now().setZone(timezone);
+    var dateDisplay = luxonObject.toLocaleString(luxon.DateTime.DATETIME_HUGE);
+    var iconCode = weatherObject.current.weather[0].icon;
+
+    var location = $('<h4>')
+        .addClass('info-location')
+        .html(locationName + '<img class="current-icon" src="http://openweathermap.org/img/wn/' + iconCode + '@2x.png"/>')
+    var date = $('<p>')
+        .addClass('current-date')
+        .text(dateDisplay);
+    var temp = $('<p>')
+        .addClass('current-temp')
+        .html('Temperature: ' + weatherObject.current.temp + ' <span>&#176;</span>F')
+    var wind = $('<p>')
+        .addClass('current-wind')
+        .text('Wind Speed: ' + weatherObject.current.wind_speed + 'mph')
+    var humidity = $('<p>')
+        .addClass('current-humidity' + '%')
+        .text('Humidity: ' + weatherObject.current.humidity);
+    var uvIndex = $('<span>')
+        .addClass('current-uv')
+        .text('UV Index: ' + weatherObject.current.uvi)
+
+    $('.weather-info').html('');
+    $('.weather-info').append(location, date, temp, wind, humidity, uvIndex);
+    
+    $('.weather-info').fadeIn(2000, function() {
+        $('.weather-info').css('display', 'flex');
+        $('.weather-info > *').each(function() {
+            $('.current-icon').css('display', 'inline');
+            $(this).fadeIn(2000, function() {
+                $(this).css('.display', 'flex');
+                
+            })
+        })
+    })
 }
 
 function create5Day(weatherObject) {
-    //console.log(weatherObject);
+    
+    let timezone = weatherObject.timezone;
+    luxonObject = luxon.DateTime.now().setZone(timezone);
+    $('.five-day > div').each(function () {
+        let increment = ($(this).attr('id'));
+        var date = luxon.DateTime.now().plus({
+            days: increment
+        });
+        var dateDisplay = date.toLocaleString(luxon.DateTime.DATE_SHORT);
+        console.log(dateDisplay);
+        $(this).text(dateDisplay);
+    })
+
+    $('.five-day').fadeIn(2000, function() {
+        $('.five-day').css('display', 'flex');
+        $('.five-day > *').each(function() {
+            $(this).fadeIn(2000, function() {
+                $(this).css('.display', 'flex');
+                
+            })
+        })
+    });
 }
 
 //Submit click event/enter
@@ -143,4 +208,5 @@ $('.search-history').on('click', 'li', function (event) {
     var itemText = ($(event.target).text());
 })
 
-retieveHistory();
+
+//retieveHistory();
