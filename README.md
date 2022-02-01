34780e01ea505508876b9e6f780ab0a93a6e5dde
Atticus-Robinson <acerobinson2@gmail.com>
2022-01-31 18:58:41 -0600

    added readme content and comments

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Nepho-Weather
+# Nepho Weather Dashboard
+
+## Intention
+
+My intention with this challenge was to create a weather dashboard that would take any location as an input. It would then display the current weather infomation for the location as well as a 5-day forcast.
+
+## Challenges
+
+I had a lot of points in the creation of this page that I was really stuck on. The first was using asynchronous functions. My difficulty with async functions came in two parts; understanding how to work around await and then, as well as getting data out of an async function. My second big challenge was the creation of the fade out animation. The fade in animation was pretty simple, but getting the timing right so that the user would never see the displayed information change was a struggle.
+
+## Externals Used
+
+Jquery - Used for almost entire document; element   creation and deletion, iteration over elements.
+
+Materialize - CSS Framework, used mainly for history list grid layout, coloring, text coloring, and some component creation
+
+Luxon - Time manipulation library, used to create a workable time object to display various time formats
+
+Google Fonts - Header and body fonts
+
+Google Geocode API - Used for location object creation from user input, which in turn was used for display text, coordinates, and storage keys
+
+Open Weather One Call API - Used for weather object creation based on coordinates. Object was used for all weather data in document
+
+## Future Improvements
+
+- Fix randomness of history item display
+- Fix history item display time lag (both issues are fetch retrieval async issues)
+
+## Function Structure
+
+- retrieveHistory() 
+    - convert()
+        - fetch(Geocode API)
+    - createHistoryItem() 
+        - storeHistoryItem()
+- submit click or enter press
+    - displayWeather()
+        - convert()
+            - fetch(Geocode API)
+        - createHistoryItem()
+        - getWeatherInfo() 
+            - fetch(Open Weather API)
+        - createInfoPage()
+            - outFade()
+            - inFade()
+        - create5Day()
+            - outFade()
+            - inFade()
+
+## Page Images
+
+![Main Page](./assets/images/Image1.png)
+
+Deployed at
+
+https://atticus-robinson.github.io/Nepho-Weather/
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ const coordinates = {};
 const weather = {};
 const historyArray = [];
 
+//Take a location input, pass to Geocode API, create Geocode object, return object
 async function convert(location) {
     var googApi = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=AIzaSyD3SFsjzLRJjuS7MuUlRnfQT8kgo7iMIaQ'
     var fetchReponse = await fetch(googApi);
@@ -18,6 +19,7 @@ async function convert(location) {
     return googleData;
 }
 
+//Take Geocode object, store coordinates, fetch Weather object, return object
 async function getWeatherInfo(googleObject) {
 
     coordinates.lat = googleObject.results[0].geometry.location.lat;
@@ -30,6 +32,7 @@ async function getWeatherInfo(googleObject) {
     return weatherData;
 }
 
+//Take input text, convert to Geocode object, createHistory item from text, convert Geocode object to Weather object, create page displays
 async function displayWeather(text) {
     let googleData = await convert(text);
     createHistoryItem(googleData);
@@ -39,6 +42,7 @@ async function displayWeather(text) {
     create5Day(weatherData, locationName);
 }
 
+//Take a Geocode object, and create a history item
 function createHistoryItem(googleDataObject) {
     var same = 0;
     var i = 0;
@@ -50,6 +54,8 @@ function createHistoryItem(googleDataObject) {
         admin1: '',
         country: ''
     };
+
+    //For each address component in the object, store the item into the correspoding key in const object
     googleDataObject.results[0].address_components.forEach(function (item) {
         switch (item.types[0]) {
             case 'neighborhood':
@@ -68,7 +74,10 @@ function createHistoryItem(googleDataObject) {
                 break;
         }
     })
+
+    //Create array to display onto page
     var displayArray = [object.neighborhood, object.city, object.admin1, object.country]
+    //If array item isn't the last item, append a ','. This will create a readable line e.g Nashville Tennesee US => Nashville, Tennesee, US
     while (i < displayArray.length) {
         if (i < (displayArray.length - 1)) {
             if (displayArray[i]) {
@@ -79,6 +88,8 @@ function createHistoryItem(googleDataObject) {
         }
         i++;
     }
+
+    //If the item to be stored already exists in the list, change same for check later
     $(".collection > li").each(function () {
         var check = ($(this).text());
         if (check === string) {
@@ -86,35 +97,49 @@ function createHistoryItem(googleDataObject) {
             return same;
         }
     });
+
+    //If there is no matched item, create <li>
     if (same === 0) {
         var historyItem = $('<li>')
             .addClass('collection-item sort name')
             .attr('name', string)
             .text(string);
+
+        //For each value in the object, if it exists, push it into the short array    
         for (var x in object) {
             if (object[x]) {
                 shortArray.push(object[x]);
             }
 
         }
+
+        //Take the first two items in the shortArray and pass to storeHistoryItem for storage in local storage. This will increase the likelyhood of a unique storage item in the case of similar location name e.g. Madison WI and Madison TN
         storeHistoryItem(shortArray[0], shortArray[1]);
+
+        //Add item to search ul
         $('.search-history').prepend(historyItem);
     } else {
         return same;
     }
 }
 
+//Store history item based on two smallest specified areas e.g [Brooklyn] []
 function storeHistoryItem(arrayItem, arrayItem2) {
-    let storeItem = arrayItem + ' ' + arrayItem2;
+    let storeItem = `${arrayItem} ${arrayItem2}`;
     historyArray.unshift(storeItem);
     var string = JSON.stringify(historyArray);
     localStorage.setItem('nepho-history', string);
 }
 
+//Retrieve local storage 
 function retieveHistory() {
     var storage = localStorage.getItem('nepho-history');
+
+    //Create array from storage parse
     var array = JSON.parse(storage);
-    array.forEach(function (item, index) {
+
+    //For each array item - convert to Google Geocode Object and call pass into createHistoryItem
+    array.forEach(function (item) {
         convert(item).then(function (response) {
             createHistoryItem(response);
         });
@@ -124,36 +149,52 @@ function retieveHistory() {
 
 }
 
+//Create detailed weather page for current time at location
 function createInfoPage(weatherObject, locationName) {
+
+    //Store timezone for usage in Luxon object
     let timezone = weatherObject.timezone;
+
+    //Create Luxon object from timezone
     luxonObject = luxon.DateTime.now().setZone(timezone);
+
+    //Store date display from Luxon object 'DATETIME_HUGE' e.g. Friday, October 14, 1983, 1:30 PM Eastern Daylight Time
     var dateDisplay = luxonObject.toLocaleString(luxon.DateTime.DATETIME_HUGE);
+
+    //Store icon code from weather object
     var iconCode = weatherObject.current.weather[0].icon;
+
+    //Set fade panel status for use in checking animation. Value is depended on what the last animation was, either in or out
     fadeStatus = $('.fade-panel').attr('fade-status');
 
+    //If fadeStatus === in (there was already an animation run, content is on the page). This ensures that the next fade in doesn't run until the fade out completes.
     if (fadeStatus === 'in') {
+        //Start fade out
         outFade()
+
+        //After duration of fade out, run fade in and change text. If text is changed before fade out completes the user will see the change
         setTimeout(function () {
             inFade();
             var location = $('<h4>')
                 .addClass('info-location')
-                .html(locationName + '<img class="current-icon" src="http://openweathermap.org/img/wn/' + iconCode + '@2x.png"/>')
+                .html(`${locationName}<img class="current-icon" src="http://openweathermap.org/img/wn/${iconCode}@2x.png"/>`)
             var date = $('<p>')
                 .addClass('current-date')
                 .text(dateDisplay);
             var temp = $('<p>')
                 .addClass('current-temp')
-                .html('Temperature: ' + weatherObject.current.temp + ' <span>&#176;</span>F')
+                .html(`Temperature: ${weatherObject.current.temp} <span>&#176;</span>F`)
             var wind = $('<p>')
                 .addClass('current-wind')
-                .text('Wind Speed: ' + weatherObject.current.wind_speed + 'mph')
+                .text(`Wind Speed: ${weatherObject.current.wind_speed}mph`)
             var humidity = $('<p>')
                 .addClass('current-humidity' + '%')
-                .text('Humidity: ' + weatherObject.current.humidity);
+                .text(`Humidity: ${weatherObject.current.humidity}`);
+            //Check UV against danger range
             uvChecker(weatherObject.current.uvi);
             var uvIndex = $('<p>')
                 .addClass('current-uv ')
-                .html(uvCat + ' UV Index: <span class="' + uvColor + ' ' + uvText + ' uvSpan">' + weatherObject.current.uvi + "</span>");
+                .html(`${uvCat} UV Index: <span class="${uvColor} ${uvText} uvSpan">${weatherObject.current.uvi}</span>`);
 
 
             $('.containerW').html('');
@@ -161,30 +202,28 @@ function createInfoPage(weatherObject, locationName) {
         }, 1100)
     }
 
-
-
-
+    //If there is no fade status present, no animations have been run. No need for fade out
     if (!fadeStatus) {
         inFade();
         var location = $('<h4>')
             .addClass('info-location')
-            .html(locationName + '<img class="current-icon" src="http://openweathermap.org/img/wn/' + iconCode + '@2x.png"/>')
+            .html(`${locationName}<img class="current-icon" src="http://openweathermap.org/img/wn/${iconCode}@2x.png"/>`)
         var date = $('<p>')
             .addClass('current-date')
             .text(dateDisplay);
         var temp = $('<p>')
             .addClass('current-temp')
-            .html('Temperature: ' + weatherObject.current.temp + ' <span>&#176;</span>F')
+            .html(`Temperature: ${weatherObject.current.temp} <span>&#176;</span>F`)
         var wind = $('<p>')
             .addClass('current-wind')
-            .text('Wind Speed: ' + weatherObject.current.wind_speed + 'mph')
+            .text(`Wind Speed: ${weatherObject.current.wind_speed}mph`)
         var humidity = $('<p>')
             .addClass('current-humidity' + '%')
-            .text('Humidity: ' + weatherObject.current.humidity);
+            .text(`Humidity: ${weatherObject.current.humidity}`);
         uvChecker(weatherObject.current.uvi);
         var uvIndex = $('<p>')
             .addClass('current-uv ')
-            .html(uvCat + ' UV Index: <span class="' + uvColor + ' ' + uvText + ' uvSpan">' + weatherObject.current.uvi + "</span>");
+            .html(`${uvCat} UV Index: <span class="${uvColor} ${uvText} uvSpan">${weatherObject.current.uvi}</span>`);
 
 
 
@@ -193,14 +232,26 @@ function createInfoPage(weatherObject, locationName) {
     }
 }
 
+//Create 5-day weather projection
 function create5Day(weatherObject) {
+
+    //Store timezone for usage in Luxon object
     let timezone = weatherObject.timezone;
+
+    //Create Luxon object from timezone
     luxonObject = luxon.DateTime.now().setZone(timezone);
+
+    //If fadeStatus === in (there was already an animation run, content is on the page). This ensures that the next fade in doesn't run until the fade out completes.
     if (fadeStatus === 'in') {
         setTimeout(function () {
+            
+            //For each div under 5 day container
             $('.five-day > div').each(function () {
+                
+                //Get day# from id
                 let increment = ($(this).attr('id'));
 
+                //Create Luxon object from selected day (increment days in the future)
                 var date = luxon.DateTime.now().plus({
                     days: increment
                 });
@@ -211,27 +262,29 @@ function create5Day(weatherObject) {
                 var iconCode = weatherObject.daily[increment].weather[0].icon;
                 var icon = $('<img>')
                     .addClass('card-icon')
-                    .attr('src', 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png')
+                    .attr('src', `http://openweathermap.org/img/wn/${iconCode}@2x.png`)
 
                 var high = weatherObject.daily[increment].temp.max;
                 var low = weatherObject.daily[increment].temp.min;
                 var temp = $('<p>')
                     .addClass('card-temp')
-                    .text(low + '/' + high);
+                    .text(`L${low}/H${high}`);
 
                 var wind = $('<p>')
                     .addClass('card-wind')
-                    .text(weatherObject.daily[increment].wind_speed + 'mph')
+                    .text(`${weatherObject.daily[increment].wind_speed}mph`)
 
                 var humidity = $('<p>')
                     .addClass('card-humidity')
-                    .text(weatherObject.daily[increment].humidity + '%')
+                    .text(`${weatherObject.daily[increment].humidity}%`)
 
                 $(this).html('');
                 $(this).append(dateDisplay, icon, temp, wind, humidity);
             })
         }, 1490)
     }
+
+    //If there is no fade status present, no animations have been run. No need for fade out
     if (!fadeStatus) {
         $('.five-day > div').each(function () {
             let increment = ($(this).attr('id'));
@@ -246,21 +299,21 @@ function create5Day(weatherObject) {
             var iconCode = weatherObject.daily[increment].weather[0].icon;
             var icon = $('<img>')
                 .addClass('card-icon')
-                .attr('src', 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png')
+                .attr('src', `http://openweathermap.org/img/wn/${iconCode}@2x.png`)
 
             var high = weatherObject.daily[increment].temp.max;
             var low = weatherObject.daily[increment].temp.min;
             var temp = $('<p>')
                 .addClass('card-temp')
-                .text(low + '/' + high);
+                .text(`L ${low}/ H ${high}`);
 
             var wind = $('<p>')
                 .addClass('card-wind')
-                .text(weatherObject.daily[increment].wind_speed + 'mph')
+                .text(`${weatherObject.daily[increment].wind_speed}mph`)
 
             var humidity = $('<p>')
                 .addClass('card-humidity')
-                .text(weatherObject.daily[increment].humidity + '%')
+                .text(`${weatherObject.daily[increment].humidity}%`)
             $(this).html('');
 
             $(this).append(dateDisplay, icon, temp, wind, humidity);
@@ -268,7 +321,7 @@ function create5Day(weatherObject) {
     }
 }
 
-
+//Fade in each 5day card and info panel
 function inFade() {
     for (let i = 1; i < 6; i++) {
         $('.five-day > #' + i)
@@ -279,6 +332,7 @@ function inFade() {
 
 }
 
+//Fade out each 5day card and info panel
 function outFade() {
     for (let i = 1; i < 6; i++) {
         $('.five-day > #' + i)
@@ -288,12 +342,10 @@ function outFade() {
     $('.fade-panel').fadeOut(1000).attr('fade-status', 'out');
 }
 
+//Check uvIndex, then store danger level, color of display span, and text color of display span. Colors are materialize classes
 function uvChecker(uvIndex) {
     uvIndex = parseInt(uvIndex);
-    console.log(uvIndex);
-    console.log(uvCat + ' ' + uvColor + ' ' + uvText);
     if (uvIndex <= 2) {
-        console.log(uvIndex)
         uvCat = 'Low';
         uvColor = 'green';
         uvText = 'white-text'
@@ -314,10 +366,9 @@ function uvChecker(uvIndex) {
         uvColor = 'black';
         uvText = 'white';
     }
-    console.log(uvCat + ' ' + uvColor + ' ' + uvText);
 }
 
-//Submit click event/enter
+//Submit click event
 $('#search-button').on('click', function () {
     searchText = $('#location').val();
     $('#location').val('');
@@ -325,6 +376,7 @@ $('#search-button').on('click', function () {
         displayWeather(searchText);
     }
 })
+//Enter key event
 $('#location').keypress(function (e) {
     if (e.which == 13) {
         searchText = $('#location').val();
@@ -335,11 +387,14 @@ $('#location').keypress(function (e) {
     }
 });
 
+//When a search history item is clicked, display weather for that location
 $('.search-history').on('click', 'li', function (event) {
     event.preventDefault();
     var itemText = ($(event.target).text());
     displayWeather(itemText);
 })
 
-
-retieveHistory();
+//When document is ready, load local storage items
+$('document').ready(function () {
+    retieveHistory();
+})
